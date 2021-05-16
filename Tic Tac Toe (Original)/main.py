dcac717c87ce5487d92e8c5b29d729a86a981836
Amir M Ghadimi <amirmohammadghadimi8@gmail.com>
2021-05-16 15:15:50 +0430

    Enhanced Constant stuff

--- Tic Tac Toe (Original)/main.py
+++ Tic Tac Toe (Original)/main.py
@@ -4,18 +4,21 @@
 import pygame
 
 pygame.font.init()
-width = 600
-height = 700
-hoffset = 100
-win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Tic Tac Toe")
-# smallfont = pygame.font.SysFont("comicsans", 20)
-font = pygame.font.SysFont("comicsans", 40)
-largefont = pygame.font.SysFont("comicsans", 65)
+
+WIDTH = 600
+HEIGHT = 700
+HOFFSET = 100
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tic Tac Toe")
+
+# smallfont = pygame.font.SysFont("comicsans", 20)
+font = pygame.font.SysFont("comicsans", 40)
+largefont = pygame.font.SysFont("comicsans", 65)
 
 
 class Game:
@@ -29,9 +32,9 @@ class Game:
         return False if (x, y) in self.moves else True
 
     def move(self, pos):
-        if 0 <= pos[0] <= width and hoffset <= pos[1] <= height:
-            x = pos[0] * 3 // width
-            y = (pos[1] - hoffset) * 3 // (height - hoffset)
+        if 0 <= pos[0] <= WIDTH and HOFFSET <= pos[1] <= HEIGHT:
+            x = pos[0] * 3 // WIDTH
+            y = (pos[1] - HOFFSET) * 3 // (HEIGHT - HOFFSET)
             if self.checkValid(x, y):
                 self.moves.add((x, y))
                 if self.turn == 1:
@@ -73,28 +76,28 @@ def redrawWindow(surface, game):
     surface.fill(WHITE)
     turn = game.turn
     text = font.render(f"Player {turn} it\'s your turn", 1, BLACK)
-    surface.blit(text, (width // 2 - text.get_width() // 2,
-                        hoffset // 2 - text.get_height() // 2))
+    surface.blit(text, (WIDTH // 2 - text.get_width() // 2,
+                        HOFFSET // 2 - text.get_height() // 2))
     for i in range(2):
-        pygame.draw.line(surface, BLACK, ((i + 1) * width // 3, hoffset),
-                         ((i + 1) * width // 3, height))
+        pygame.draw.line(surface, BLACK, ((i + 1) * WIDTH // 3, HOFFSET),
+                         ((i + 1) * WIDTH // 3, HEIGHT))
         pygame.draw.line(surface, BLACK,
-                         (0, ((i + 1) * (height - hoffset) // 3) + hoffset),
-                         (width,
-                          ((i + 1) * (height - hoffset) // 3) + hoffset))
+                         (0, ((i + 1) * (HEIGHT - HOFFSET) // 3) + HOFFSET),
+                         (WIDTH,
+                          ((i + 1) * (HEIGHT - HOFFSET) // 3) + HOFFSET))
 
     for move in game.player1.moves:
-        circleMiddle = ((move[0] * width / 3) + width / 6,
-                        (move[1] * (height - hoffset) / 3) +
-                        (height - hoffset) / 6 + hoffset)
-        pygame.draw.circle(surface, BLUE, circleMiddle, 2 * width // 15, 4)
+        circleMiddle = ((move[0] * WIDTH / 3) + WIDTH / 6,
+                        (move[1] * (HEIGHT - HOFFSET) / 3) +
+                        (HEIGHT - HOFFSET) / 6 + HOFFSET)
+        pygame.draw.circle(surface, BLUE, circleMiddle, 2 * WIDTH // 15, 4)
         del circleMiddle
 
-    radius = 2 * width // 15
+    radius = 2 * WIDTH // 15
     for move in game.player2.moves:
-        circleMiddle = ((move[0] * width / 3) + width / 6,
-                        (move[1] * (height - hoffset) / 3) +
-                        (height - hoffset) / 6 + hoffset)
+        circleMiddle = ((move[0] * WIDTH / 3) + WIDTH / 6,
+                        (move[1] * (HEIGHT - HOFFSET) / 3) +
+                        (HEIGHT - HOFFSET) / 6 + HOFFSET)
         pygame.draw.line(surface, RED,
                          (circleMiddle[0] - radius, circleMiddle[1] - radius),
                          (circleMiddle[0] + radius, circleMiddle[1] + radius),
@@ -130,20 +133,20 @@ def main():
 
         if winner == 1:
             text = largefont.render("Player 1 Wins, Congrats!!!", 1, RED)
-            win.blit(text, (width // 2 - text.get_width() // 2,
-                            height // 2 - text.get_height() // 2))
+            win.blit(text, (WIDTH // 2 - text.get_width() // 2,
+                            HEIGHT // 2 - text.get_height() // 2))
             run = False
 
         elif winner == 2:
             text = largefont.render("Player 2 Wins, Congrats!!!", 1, RED)
-            win.blit(text, (width // 2 - text.get_width() // 2,
-                            height // 2 - text.get_height() // 2))
+            win.blit(text, (WIDTH // 2 - text.get_width() // 2,
+                            HEIGHT // 2 - text.get_height() // 2))
             run = False
 
         elif winner == -1:
             text = largefont.render("Oops Looks Like a Tie!!!", 1, RED)
-            win.blit(text, (width // 2 - text.get_width() // 2,
-                            height // 2 - text.get_height() // 2))
+            win.blit(text, (WIDTH // 2 - text.get_width() // 2,
+                            HEIGHT // 2 - text.get_height() // 2))
             run = False
 
         pygame.display.update()
@@ -159,8 +162,8 @@ def main_menu(surface):
         win.fill(WHITE)
         clock.tick(20)
         text = largefont.render("Click to start the GAME XD", 1, RED)
-        win.blit(text, (width // 2 - text.get_width() // 2,
-                        height // 2 - text.get_height() // 2))
+        win.blit(text, (WIDTH // 2 - text.get_width() // 2,
+                        HEIGHT // 2 - text.get_height() // 2))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
